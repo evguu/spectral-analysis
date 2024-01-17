b016cc68d9b812f1b426f61685ea6e22d97961e5
Eugene Savchenko <eugene.savchenko@succraft.com>
2024-01-17 14:17:02 +0300

    Added playback.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # Given audio file path (mp3 or wav), load it and print the sample count.
-import pydub
+from pydub import AudioSegment
+from pydub.playback import play
 
-
+sound = AudioSegment.from_file("input/audio.mp3", format="mp3")
+play(sound)
