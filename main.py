8a80de86555adf14d9aa4f2284c54d5a364499ee
Eugene Savchenko <eugene.savchenko@succraft.com>
2024-01-17 14:42:11 +0300

    Added fft from scipy.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Given audio file path (mp3 or wav), load it and print the sample count.
 from pydub import AudioSegment
-from pydub.playback import play
+from scipy.fft import fft, fftfreq
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def open_audio_file(file_path):
@@ -10,7 +12,16 @@ def open_audio_file(file_path):
 
 def main():
     sound = open_audio_file("input/audio.mp3")
-    play(sound)
+    graph_wav_freq(sound.get_array_of_samples(), sound.frame_rate)
+
+
+def graph_wav_freq(np_arr, sample_rate):
+    N = len(np_arr)
+    yf = fft(np_arr)
+    xf = fftfreq(N, 1 / sample_rate)
+
+    plt.plot(xf, np.abs(yf))
+    plt.show()
 
 
 if __name__ == "__main__":
