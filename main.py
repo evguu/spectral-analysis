7ba7e18b9d6c0ad4af262cdd14e827bdec28eb1a
Eugene Savchenko <eugene.savchenko@succraft.com>
2024-01-17 15:08:15 +0300

    Added output (no changes applied)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Given audio file path (mp3 or wav), load it and print the sample count.
 from pydub import AudioSegment
-from scipy.fft import fft, fftfreq
+from pydub.playback import play
+from scipy.fft import rfft, rfftfreq, irfft
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
 
 
 def open_audio_file(file_path):
@@ -12,17 +14,22 @@ def open_audio_file(file_path):
 
 def main():
     sound = open_audio_file("input/audio.mp3")
-    graph_wav_freq(sound.get_array_of_samples(), sound.frame_rate)
+    fft_res = graph_wav_freq(sound.get_array_of_samples(), sound.frame_rate)
+    new_sig = irfft(fft_res)
+    norm_new_sig = np.int16(new_sig * (32767 / new_sig.max()))
+    write("output/clean.wav", sound.frame_rate, norm_new_sig)
+
 
 
 def graph_wav_freq(np_arr, sample_rate):
     N = len(np_arr)
-    yf = fft(np_arr)
-    xf = fftfreq(N, 1 / sample_rate)
+    yf = rfft(np_arr)
+    xf = rfftfreq(N, 1 / sample_rate)
 
-    plt.plot(xf, np.abs(yf))
-    plt.show()
+    # plt.plot(xf, np.abs(yf))
+    # plt.show()
 
+    return yf
 
 if __name__ == "__main__":
     main()
