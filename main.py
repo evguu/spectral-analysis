3423460a698a757e3ad4607b77b499bd1b29a337
Eugene Savchenko <eugene.savchenko@succraft.com>
2024-01-17 15:19:33 +0300

    Added manual frequency filtering :D

--- main.py
+++ main.py
@@ -15,19 +15,30 @@ def open_audio_file(file_path):
 def main():
     sound = open_audio_file("input/audio.mp3")
     fft_res = graph_wav_freq(sound.get_array_of_samples(), sound.frame_rate)
-    new_sig = irfft(fft_res)
+    fft_flt = filter_fft(fft_res, sound.frame_rate, {4000: 1e9})
+    new_sig = irfft(fft_flt)
+    graph_wav_freq(new_sig, sound.frame_rate, True)
+
     norm_new_sig = np.int16(new_sig * (32767 / new_sig.max()))
     write("output/clean.wav", sound.frame_rate, norm_new_sig)
 
 
+def filter_fft(fft_res, sample_rate, modification_dict):
+    points_per_freq = len(fft_res) / (sample_rate / 2)
+    for freq, amp in modification_dict.items():
+        target_idx = int(points_per_freq * freq)
+        fft_res[target_idx - 1: target_idx + 2] = amp
+    return fft_res
 
-def graph_wav_freq(np_arr, sample_rate):
+
+def graph_wav_freq(np_arr, sample_rate, enable_graph=False):
     N = len(np_arr)
     yf = rfft(np_arr)
     xf = rfftfreq(N, 1 / sample_rate)
 
-    # plt.plot(xf, np.abs(yf))
-    # plt.show()
+    if enable_graph:
+        plt.plot(xf, np.abs(yf))
+        plt.show()
 
     return yf
 
